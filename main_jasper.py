809123a2f76d0af751d707e734eb137860a56d95
jaspermulder99 <jaspermulder99@hotmail.nl>
2023-06-21 01:24:48 +0200

    main jasper

--- main_jasper.py
+++ main_jasper.py
@@ -4,16 +4,4 @@ from check_and_correct import check_instances
 
 
 if __name__ == "__main__":
-    # ------- Initialization -------
-    input_matrix = np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
-    input_matrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]])
-    output_matrix = np.array([[1, 1, 0], [1, 1, 0], [0, 0, 1]])
-
-    num_V = np.shape(input_matrix)[1]
-    alpha = 1  # reward
-    beta = 4  # penalty
-
-    # ------- Solution -------
-    # solution_dict, lowest_energy = find_solution(input_matrix, alpha, beta)
-    # print(solution_dict)
-    # print(check_instances(solution_dict))
+    pass
