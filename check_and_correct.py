8a8e4310c2778db51b5d1cc33414affefdf1d434
jaspermulder99 <jaspermulder99@hotmail.nl>
2023-06-21 01:25:09 +0200

    added fix for incorrect size

--- check_and_correct.py
+++ check_and_correct.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import permutations
 
 
 def check_size(sample):
@@ -41,8 +42,57 @@ def check_order(sample):
 
 
 def fix_size(sample):
-    # e.g. [[2,0],[0],[1]] -> [[2],[0],[1]]
-    corrections = [[[2], [0], [1]], [[2], [1], [1]]]
+    corrections = []
+    len_items = np.array([len(item) for item in sample])
+
+    # The code is not made for sublists larger than 3
+    if max(len_items) >= 3:
+        print("Cannot find a fix for sample ", sample)
+        return corrections
+
+    idx_0 = np.where(len_items == 0)[0]
+    idx_1 = np.where(len_items == 1)[0]
+    idx_2 = np.where(len_items == 2)[0]
+
+    # Remove doubles.
+    # e.g. [[2,0], [0], [1]] -> [[2], [0], [1]]
+    idxs = np.concatenate((idx_0, idx_1))
+    items_without_2s = [
+        item for sublst in [sample[idx] for idx in idxs] for item in sublst
+    ]
+    new_sample = list(sample)
+    for i in idx_2:
+        new_sample[i] = [x for x in new_sample[i] if x not in items_without_2s]
+
+    # Recalculate
+    len_items = np.array([len(item) for item in new_sample])
+    idx_0 = np.where(len_items == 0)[0]
+    idx_1 = np.where(len_items == 1)[0]
+    idx_2 = np.where(len_items == 2)[0]
+
+    if len(idx_0) > len(idx_2):
+        print("Cannot find a fix for sample ", sample)
+        return corrections
+    elif len(idx_0) < len(idx_2):
+        print("Cannot find a fix for sample ", sample)
+        return corrections
+
+    # Check if number of empty spots is equal to number of sublists with size 2.
+    # If so, permute the items from sublists with size 2 over all sublsts of size 0 and 2.
+    # e.g. [[2,0], [], [1]] -> [[2], [0], [1]] and [[0], [2], [1]]
+    elif len(idx_0) == len(idx_2):
+        sample = list(new_sample)
+        idxs = np.concatenate((idx_0, idx_2))
+        items = [[item] for sublst in [sample[idx] for idx in idx_2] for item in sublst]
+        item_permutations = list(permutations(items))
+        for permutation in item_permutations:
+            new_sample = list(sample)  # copy original sample
+            for i in range(len(permutation)):
+                new_sample[idxs[i]] = permutation[i]
+            corrections.append(new_sample)
+    else:
+        corrections.append(sample)
+
     return corrections
 
 
@@ -53,7 +103,7 @@ def fix_mece(sample):
 
 
 def fix_order(sample):
-    # only if #jumps =1
+    # only if #jumps == 2, e.g. [1,0,2,3] -> [1,3,0,2]
     corrections = []
     return corrections
 
@@ -139,8 +189,15 @@ if __name__ == "__main__":
     test_dict["Sample 3"] = [[0], [0], [1]]
     test_dict["Sample 4"] = [[0], [1], [2]]
 
-    print(test_dict)
+    # print(test_dict)
     # checked = check_instances(test_dict)
-    checked, corr = check_instances(test_dict, True)
-    print(checked)
-    print(corr)
+    # checked, corr = check_instances(test_dict, True)
+    # print(checked)
+    # print(corr)
+
+    # ---- test fixes ---
+
+    print(fix_size([[2, 0], [0], [1]]))
+    print(fix_size([[2, 0], [], [1]]))
+    print(fix_size([[2, 0], [1, 3], [1], []]))
+    print(fix_size([[2, 0], [0], []]))
